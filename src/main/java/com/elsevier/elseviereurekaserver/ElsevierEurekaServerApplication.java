97b9120c96925d0209dc5046d89e6f459b46415e
An Phuc Thien Nguyen <npta97@gmail.com>
2020-07-29 13:53:41 +0700

    Init project

---
+++ src/main/java/com/elsevier/elseviereurekaserver/ElsevierEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.elsevier.elseviereurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ElsevierEurekaServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ElsevierEurekaServerApplication.class, args);
+  }
+}
---
+++ src/test/java/com/elsevier/elseviereurekaserver/ElsevierEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsevier.elseviereurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsevierEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
